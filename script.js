ec0c7b52103b5070893a0f43816923f3b0c9be56
Ramon <ramonfrombr@gmail.com>
2024-06-28 19:21:32 -0300

    Selecionou elementos

---
+++ script.js
@@ -0,0 +1,9 @@
+const numeros = document.querySelectorAll(".numeros span");
+const contador = document.querySelector(".contador");
+const mensagemFinal = document.querySelector(".final");
+const repetir = document.querySelector("#repetir");
+
+console.log(numeros);
+console.log(contador);
+console.log(mensagemFinal);
+console.log(repetir);
